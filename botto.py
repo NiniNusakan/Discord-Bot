84f5a3580f33eb5726f707d9cef093ebfb7dfe43
NiniNusakan <nicole.carvalho.s.goncalves@gmail.com>
2026-03-12 19:09:47 -0300

    botto.py

---
+++ botto.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+print(f'Estamos logados como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+await ctx.send(f'Olá! eu sou um bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+await ctx.send("he" * count_heh)
+
+@bot.event
+async def on_member_join(member):
+channel = member.guild.system_channel
+await channel.send(f'Bem-vindo ao servidor, {member.mention}!')
+
+bot.run("your token here")
